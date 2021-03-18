35dc02ad577857c18d425ebaec74ff40ca7a87ab
Daniel Suh <ds91149@uga.edu>
2021-03-17 20:10:32 -0400

    final version of lhs

--- lhs.R
+++ lhs.R
@@ -62,27 +62,34 @@ h <- 1000
 set.seed(8878896)
 lhs <- maximinLHS(h,10)
 
+K <-125000 #values arbitrarily chosen
+R0 <- 2
+mu <- 1/(365*75)
+lambda <- K*mu
+theta <- 1/5
+alpha <- 1/7
+beta <- (R0*alpha)/K
 
-beta1.min=0.00003
-beta1.max=0.0003
-beta2.min=0.00003
-beta2.max=0.0003
-lambda.min=5
-lambda.max=20
-mu.min=1/1000
-mu.max=1/100
-mu1.min=1/100
-mu1.max=1/10
+beta1.min=beta/10
+beta1.max=beta*10
+beta2.min=beta/10
+beta2.max=beta*10
+lambda.min=3
+lambda.max=10
+mu.min=1/(365*80)
+mu.max=1/(365*70)
+mu1.min=1/(365*80)
+mu1.max=1/(365*60)
 phi.min=0
 phi.max=1
 psi.min=0
 psi.max=1
-theta.min=1/7
-theta.max=1/4
-alpha.min=1/7
-alpha.max=1/14
-gamma.min=10000
-gamma.max=100000
+theta.min=1/8
+theta.max=1/3
+alpha.min=1/14
+alpha.max=1/5
+gamma.min=K*0.001
+gamma.max=K*0.01
 
 
 params.set <- cbind(
@@ -92,92 +99,51 @@ params.set <- cbind(
   mu = lhs[,4]*(mu.max-mu.min)+mu.min,
   mu1 = lhs[,5]*(mu1.max-mu1.min)+mu1.min,
   psi = lhs[,6]*(psi.max-psi.min)+psi.min,
-#  phi = lhs[,6]*(phi.max-phi.min)+phi.min,
+  #  phi = lhs[,6]*(phi.max-phi.min)+phi.min,
   theta = lhs[,7]*(theta.max-theta.min)+theta.min,
   alpha = lhs[,8]*(alpha.max-alpha.min)+alpha.min,
   gamma = lhs[,9]*(gamma.max-gamma.min)+gamma.min)
 
 
-# levels <- 11
-# 
-# h2 <- 250
-# 
-# R0 <- 2
-# threshold <- (1-1/R0)*as.numeric(xstart[1])
-# antibody.prop <- seq(0,1,0.1)
-# j <- 1  
-# data <- data.frame(matrix(rep(NA,levels*h2*11),nrow=levels*h2))
-# #technically, this loop may need to calculate R0 within it since R0 and the threshold will change with the parameters
-# #this can be done but I need to get that matrix to work
-# 
-# for(i in 1:h2){
-#   for (k in 1:length(antibody.prop)){
-#     
-#     data[j,1:10] <- params <- as.list(c(params.set[i,], phi=antibody.prop[k]))
-#     output <- as.data.frame(ode(xstart, times, de, params))
-#     output$herd <- output$S<=threshold
-#     tmp <- output %>% filter(herd==1)
-#     data[j,11] <- tmp$time[1]
-#     j <- j+1
-#     
-#   }
-# }
-# 
-# names(data) <- c(names(params),'threshold.time')
-# 
-# data %>% mutate(phi = as.factor(phi)) %>% ggplot(., aes(x=phi, y=threshold.time)) + geom_boxplot(notch = T) + geom_jitter()
-# 
-# 
-# bonferroni.alpha = 0.05/10
-# prcc <- pcc(data[,1:9], data[,11], nboot=1000, rank=T, conf=1-bonferroni.alpha)
-# print(prcc)
-
-
-############################ calculating R0 for each sim
-
-
-
 levels <- 11
 
-h2 <- 250
+h2 <- 1000
 
 antibody.prop <- seq(0,1,0.1)
 j <- 1  
-data <- data.frame(matrix(rep(NA,levels*h2*11),nrow=levels*h2))
+lhs_data <- data.frame(matrix(rep(NA,levels*h2*11),nrow=levels*h2))
 
 for(i in 1:h2){
   for (k in 1:length(antibody.prop)){
     
-    data[j,1:10] <- params <- as.list(c(params.set[i,], phi=antibody.prop[k]))
-      mat <- matrix(1:9, nrow = 3, ncol = 3)
-      mat[1] = 0
-      mat[2] = (params$beta1)*(params$lambda/params$mu)*(1/(params$theta+params$mu))
-      mat[3] = (params$beta2)*(params$lambda/params$mu)*(1/(params$theta+params$mu))
-      mat[4] = (params$psi*params$theta)/(params$alpha+params$mu1)
-      mat[5] = 0
-      mat[6] = 0
-      mat[7] = ((1-params$psi)*params$theta)/(params$alpha+params$mu1)
-      mat[8] = 0
-      mat[9] = 0
-      R0 <- max(eigen(mat)$values)
-      threshold <- (1-1/R0)*as.numeric(xstart[1])
+    lhs_data[j,1:10] <- params <- as.list(c(params.set[i,], phi=antibody.prop[k]))
+    mat <- matrix(1:9, nrow = 3, ncol = 3)
+    mat[1] = 0
+    mat[2] = (params$beta1)*(params$lambda/params$mu)*(1/(params$theta+params$mu))
+    mat[3] = (params$beta2)*(params$lambda/params$mu)*(1/(params$theta+params$mu))
+    mat[4] = (params$psi*params$theta)/(params$alpha+params$mu1)
+    mat[5] = 0
+    mat[6] = 0
+    mat[7] = ((1-params$psi)*params$theta)/(params$alpha+params$mu1)
+    mat[8] = 0
+    mat[9] = 0
+    R0 <- max(eigen(mat)$values)
+    threshold <- (1-1/R0)*as.numeric(xstart[1])
     output <- as.data.frame(ode(xstart, times, de, params))
     output$herd <- output$S<=threshold
     tmp <- output %>% filter(herd==1)
-    data[j,11] <- R0
-    data[j,12] <- tmp$time[1]
+    lhs_data[j,11] <- R0
+    lhs_data[j,12] <- tmp$time[1]
     j <- j+1
     
   }
 }
 
-names(data) <- c(names(params),'R0','threshold.time')
+names(lhs_data) <- c(names(params),'R0','threshold.time')
 
-data %>% mutate(phi = as.factor(phi)) %>% ggplot(., aes(x=phi, y=threshold.time)) + geom_boxplot()
-
-data %>% filter(threshold.time>1) %>% mutate(phi = as.factor(phi)) %>% ggplot(., aes(x=phi, y=threshold.time)) + geom_point()
+lhs_data %>% mutate(phi = as.factor(phi)) %>% ggplot(., aes(x=phi, y=threshold.time)) + geom_boxplot()
 
 
 bonferroni.alpha = 0.05/10
-prcc <- pcc(data[,1:9], data[,12], nboot=1000, rank=T, conf=1-bonferroni.alpha)
+prcc <- pcc(lhs_data[,1:10], lhs_data[,12], nboot=1000, rank=T, conf=1-bonferroni.alpha)
 print(prcc)
